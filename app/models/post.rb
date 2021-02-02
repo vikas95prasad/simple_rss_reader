cfb40deb35946b7d5e366fdd73b3be4dbde03ed9
vikas-prasad <vprasad@edcast.com>
2021-02-02 17:04:06 +0530

    Add searching post by url

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,12 +8,19 @@ class Post < ApplicationRecord
   validates_presence_of :url_hash
   validates_uniqueness_of :url_hash
 
-  before_save :create_url_hash
+  before_save :generate_url_hash
 
   scope :recents, -> { order('published_at desc') }
-  scope :by_url, ->(url) { where(url_hash: Post.create_url_hash(url)) }
+  scope :by_url, ->(url) { where(url_hash: Post.get_url_hash(url)) }
 
-  def self.create_url_hash(url)
+  def self.get_url_hash(url)
     UrlGenerator.hash(url)
+  end
+
+  private
+
+  def generate_url_hash
+    # Hash the URL, but don't hash null values
+    self[:url_hash] = (self.class.get_url_hash(self[:url]) if self[:url].present?)
   end
 end
